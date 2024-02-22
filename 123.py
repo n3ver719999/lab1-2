acb7ff1f56a01723b3c30664ab84bf55f16dd808
n3ver719999 <raydexamorte@gmail.com>
2024-02-22 11:08:14 +0300

    123

--- 123.py
+++ 123.py
@@ -1,6 +1,6 @@
 from bidict import bidict
 while True:
-    stolici = bidict({'Москва': "Россия", 'Лондон': "Англия"})
+    stolici = bidict({'Москва': "Россия", 'Лондон': "Англия",'Вашингтон': "США", 'Минск': "Беларусь"})
     a = input('Введите столицу или страну: ')
     if a in stolici:
         print(stolici[a])
